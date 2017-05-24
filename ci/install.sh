b2a6ce0232298d511231ea8cf3e8c5e7888c0eb4
Jorge Aparicio <jorge@japaric.io>
2017-05-24 00:00:44 -0500

    install ARM toolchain again

--- ci/install.sh
+++ ci/install.sh
@@ -1,6 +1,10 @@
 set -ex
 
 main() {
+    local url=https://developer.arm.com/-/media/Files/downloads/gnu-rm/6_1-2017q1/gcc-arm-none-eabi-6-2017-q1-update-linux.tar.bz2
+
+    curl -L $url | tar -C $GCC_DIR --strip-components 1 -xj
+
     ( cd .. && cargo install xargo || true )
     rustup component add rust-src
 }
